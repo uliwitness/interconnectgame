118eb29db484a597f34889d4471a002d451297cf
Uli Kusterer <ulivc@zathras.de>
2014-11-24 11:24:11 +0100

    Initial Commit

---
+++ interconnectgame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  interconnectgame
+//
+//  Created by Uli Kusterer on 2014-11-24.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ interconnectgame/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  interconnectgame
+//
+//  Created by Uli Kusterer on 2014-11-24.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
