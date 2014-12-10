a4c53b6094a5b7fb8d33e784bd1265bafb3a468f
Uli Kusterer <ulivc@zathras.de>
2014-12-10 04:00:37 +0100

    More comments so I remember what the Lua-hosting code does.

--- interconnectserver/interconnect_scripts.cpp
+++ interconnectserver/interconnect_scripts.cpp
@@ -25,6 +25,7 @@ using namespace eleven;
 static std::string	sSettingsFolderPath;
 
 
+// An example C function that we call from Lua:
 static int foo (lua_State *L)
 {
 	int n = lua_gettop(L);    /* number of arguments */
@@ -44,6 +45,8 @@ static int foo (lua_State *L)
 	return 2;                   /* number of results */
 }
 
+
+// C++ closure (lambda, block, whatever) that we register with our server that runs a Lua script:
 eleven::handler		runscript = []( session_ptr session, std::string currRequest, chatserver* server )
 {
 	user_session_ptr	loginInfo = session->find_sessiondata<user_session>(USER_SESSION_DATA_ID);		
@@ -66,35 +69,41 @@ eleven::handler		runscript = []( session_ptr session, std::string currRequest, c
 	filePath.append( fileName );
 	filePath.append( ".lua" );
 	
-	lua_State *L = luaL_newstate();
+	// ===== LUA CODE START: =====
+	lua_State *L = luaL_newstate();	// Create a context.
 	
-	luaL_openlibs(L);
+	luaL_openlibs(L);	// Load Lua standard library.
 	
-	lua_newtable( L );
-	lua_setglobal( L, "eleven_obj" );
-	lua_getglobal( L, "eleven_obj" );
-	lua_pushcfunction( L, foo );
-	lua_setfield( L, -2, "durchschnitt" );
-	lua_pop( L, 1 );
+	// CREATE A C-backed Lua object:
+	lua_newtable( L );	// Create a new object & push it on the stack.
+	lua_pushcfunction( L, foo );	// Create an (unnamed) function with C function "foo" as the implementation.
+	lua_setfield( L, -2, "durchschnitt" );	// Pop the function off the back of the stack and into the object (-2 == penultimate object on stack) using the key "durchschnitt" (i.e. method name).
+	lua_setglobal( L, "eleven_obj" );	// Pop the object off the stack into a global named "eleven_obj".
 	
-	lua_register( L, "myavg", foo );
+	// CREATE A C-BACKED LUA FUNCTION:
+	lua_register( L, "myavg", foo );	// Create a global named "myavg" and stash an unnamed function with C function "foo" as its implementation in it.
 
+	// Load the file:
 	int s = luaL_loadfile( L, filePath.c_str() );
 
 	if( s == 0 )
 	{
-		// execute Lua program
+		// Run it, with 0 params, accepting an arbitrary number of return values.
+		//	Last 0 is error handler Lua function's stack index, or 0 to ignore.
 		s = lua_pcall(L, 0, LUA_MULTRET, 0);
 	}
 
+	// Was an error? Get error message off the stack and send it back:
 	if( s != 0 )
 	{
 		session->printf( "/!script_error %s\r\n", lua_tostring(L, -1) );
 		lua_pop(L, 1); // remove error message
 	}
 	else
-		session->printf( "/ran_script\r\n" );
-	lua_close(L);
+		session->printf( "/ran_script\r\n" );	// Send back indication of success.
+	lua_close(L);	// Dispose of the script context.
+	
+	// ===== LUA CODE END. =====
 };
 	
 
